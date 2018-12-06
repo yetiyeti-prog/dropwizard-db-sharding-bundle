8a8a49b9c308da6237ba1d2243b286c06e9bdc80
Avanish Pandey <avanish.pandey@phonepe.com>
2018-12-06 15:01:31 +0530

    removed unwanted import

--- src/main/java/io/dropwizard/sharding/DBShardingBundle.java
+++ src/main/java/io/dropwizard/sharding/DBShardingBundle.java
@@ -38,7 +38,6 @@ import io.dropwizard.sharding.sharding.ShardManager;
 import io.dropwizard.sharding.sharding.impl.ConsistentHashBucketIdExtractor;
 import lombok.Getter;
 import lombok.val;
-import org.assertj.core.util.Strings;
 import org.hibernate.SessionFactory;
 import org.reflections.Reflections;
 
